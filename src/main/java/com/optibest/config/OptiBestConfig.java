128b56fc2042960ad10a23ca027fa0f2916bcfb4
mkivanc27-boop <mkivanc27@gmail.com>
2026-04-08 03:37:12 +0300

    Update OptiBestConfig.java

--- src/main/java/com/optibest/config/OptiBestConfig.java
+++ src/main/java/com/optibest/config/OptiBestConfig.java
@@ -1,29 +1,9 @@
 package com.optibest.config;
 
 public class OptiBestConfig {
-    public static boolean entityCulling = true;
-    public static boolean frustumCulling = true;
     public static boolean fogDisabled = true;
-    public static boolean entityShadowsDisabled = true;
-    public static boolean cloudRenderOff = true;
-    public static boolean skyRenderOff = true;
-    public static boolean weatherRenderOff = true;
-    public static boolean chunkThrottling = true;
-    public static boolean smoothLightingOff = false;
-    public static boolean chunkCacheOptimization = true;
     public static boolean particleLimit = true;
     public static int maxParticles = 10;
-    public static boolean blockEntityCulling = true;
-    public static boolean biomeBlendsOff = true;
-    public static boolean unlimitedFps = true;
-    public static int targetViewDistance = 4;
-    public static boolean fastGraphics = true;
-    public static boolean entityTickOptimization = true;
-    public static boolean memoryOptimization = true;
-    public static boolean soundThrottle = true;
-    public static boolean hudOptimization = true;
-    public static boolean mobAiThrottle = true;
-    public static boolean shadowBypass = true;
-    public static boolean playerListOptimization = true;
-    public static boolean packetThrottle = true;
+    public static boolean weatherRenderOff = true;
+    public static boolean cloudRenderOff = true;
 }
